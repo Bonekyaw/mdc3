2c3d1bcb34c4e0ec5ce32bb1e354da06e6863390
bonekyaw <phonenai2014@gmail.com>
2025-02-16 17:18:09 +0630

    patch method

--- backend-sample/src/routes/v1/user.ts
+++ backend-sample/src/routes/v1/user.ts
@@ -20,7 +20,7 @@ router.get("/products", index);
 // GET localhost:8080/api/v1/products/1234 - get a product
 router.get("/products/:id", show);
 
-// POST localhost:8080/api/v1/products/add-favourite
-router.post("/products/favourite-toggle", toggleFavourite);
+// PATCH localhost:8080/api/v1/products/add-favourite
+router.patch("/products/favourite-toggle", toggleFavourite);
 
 export default router;
